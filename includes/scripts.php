a343de6e6219d0860a2491cd9e17c1c89e95676d
Thomas Wheeler <wheelert@wheelerwire.com>
2024-11-04 19:34:18 -0800

    added donation link

--- includes/scripts.php
+++ includes/scripts.php
@@ -158,6 +158,22 @@ function ww_rf_options_page_html() {
             
 			?>
 		</form>
+        <div id="donate-button-container">
+            <div id="donate-button"></div>
+            <script src="https://www.paypalobjects.com/donate/sdk/donate-sdk.js" charset="UTF-8"></script>
+            <script>
+            PayPal.Donation.Button({
+            env:'production',
+            hosted_button_id:'AH4J42WEFCSDS',
+            image: {
+            src:'https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif',
+            alt:'Donate with PayPal button',
+            title:'PayPal - The safer, easier way to pay online!',
+            }
+            }).render('#donate-button');
+            </script>
+</div>
+
 	</div>
 	<?php
 }
